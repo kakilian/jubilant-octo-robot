46c62763058f9ca1e723328ace33c24fa53e53fd
Katarina Kilian <khkilian22@gmail.com>
2024-05-16 08:18:10 +0000

    intial function add

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+
+function initalizeQuiz {
+// Set up eventlistener, initalize var.//
+} 
+
+function displayQuestion {
+//Display Questions and Answers //
+}
+
+function checkAnswer {
+//Check the selected answers with correct asnwer for current questions//
+}
+
+function displayResult {
+// Display whether users answer was correct or incorrect //
+}
+
+function updateScore {
+// updated score with users correct answers //
+}
+
+function displayNextQuestion {
+//Display next question //
+}
+
+function displayFinalScore {
+// Display final score with information about cats and alice in wonderland fairytale //
+}
+
+function resetQuiz {
+// reset Quiz, including score, and players name //
+}
+
+function schuffleArray {
+// shuffle the order of the questions and answer choices //
+}
+
+initalizeQuiz(); 
+//initalize the quiz //
+
