d45d8196dbcd76d1b379382a12df1caf54fe7738
Katarina Kilian <khkilian22@gmail.com>
2024-05-27 12:42:33 +0000

    updating js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,50 @@
-/* jshint esversion: 8 */
 console.log('script loaded');
 console.log('script.js');
 console.log('index.js');
-/**
- *  Define and log key DOM elements
- */
+
+let questions = {};
+let correctAnswer = {};
+let incorrectAnswers = {};
+/** 
+ * DOM Loading
+*/ 
+document.addEventListener('DOMContentLoaded', () => {
+    startButton.addEventListener('click', startGame);
+    const introductionContainer = document.getElementById('introduction-container'); 
+    const startButton = document.getElementById('start-btn-front');
+    const questionContainerElement = document.getElementById('question-container');
+    introductionContainer.classList.remove('hide');
+    questionContainerElement.classList.add('hide');
+    nextButton.addEventListener.classList.add('hide');
+    setNextQuestion();
+});
+
+function startGame () {
+    introductionContainer.classList.add('hide');
+    currentQuestions = questions.sort(() => Math.random() -0.5).slice(0, 10);
+    currentQuestionsIndex = 0;
+    score = 0;   
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion ();
+}
+
+function setNextQuestion () {
+    introductionContainer.classList.add('hide');
+    showAnswer(shuffledAnswers[currentAnswersIndex]);
+    resetState();
+}
+
+function resetState () {
+    nextButton,classList.add('hide');
+    correctAnswerElement.classList.add('hide');
+    incorrectAnswers.classList.add('hide');
+}
+
+function showAnswer () {
+
+
+}
+
+function results() {
+
+}
