4b98e86847868229d37388105e97bd3d0cb4e9e1
Katarina Kilian <khkilian22@gmail.com>
2024-06-09 06:20:53 +0000

    Leadersboard updated

--- assets/js/players.js
+++ assets/js/players.js
@@ -3,7 +3,6 @@ console.log('score-board');
 /**
  *  Player Class as a blueprint for creating player objects, Own name, date and score
  */
-
 class Player {
     constructor(name, date, score) {
         this.name = name;
@@ -15,7 +14,6 @@ class Player {
 /**
  *  Player Array, showing past players and scores on this quiz
  */
-
 let player = [
     new Player('Violet', '2024-06-11', 9),
     new Player('Charly', '2024-06-08', 10),
@@ -27,9 +25,6 @@ let player = [
 /**
  * Generating a table, sorted by player scores, in desending order, each player has their own row
  */
-
-//let players = [play1, play2, play3, play4];
-
 export function displayLeaderboard() {
     let leaderboardHTML = "<table><tr><th>Name</th><th>Score</th></tr>";
     player.sort((aPlayer, bPlayer) => bPlayer.score - aPlayer.score);
@@ -45,7 +40,6 @@ export function displayLeaderboard() {
  * @param {*} date 
  * @param {*} score 
  */
-
 export function addPlayer(name, date, score) {
     player.push(new Player(name, date, score));
     displayLeaderboard();
